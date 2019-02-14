f5a30b171cb02100477cf110b89a078335ba0fd4
Alex Vuong <avuong2962@gmail.com>
2019-02-13 17:05:29 -0700

    Update name and documentatation of hatch placer
    Rename HatchPlacer --> HatchPlacerSubsystem
    Add documentation
    Rename scissors methods to secureHatch() and releaseHatch()

--- src/main/java/frc/robot/subsystems/HatchPlacerSubsystem.java
+++ src/main/java/frc/robot/subsystems/HatchPlacerSubsystem.java
@@ -4,8 +4,10 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-//TODO documentation
-public class HatchPlacer extends Subsystem {
+/**
+ * Contains the pneumatics required to place hatches on the velcro targets.
+ */
+public class HatchPlacerSubsystem extends Subsystem {
 
     private static final int MAIN_PCM = 1;
     private static final int EXTRA_PCM = 2;
@@ -19,31 +21,51 @@ public class HatchPlacer extends Subsystem {
 
     private DoubleSolenoid scissorHolder, leftLauncher, rightLauncher;
 
-    public HatchPlacer() {
+    /**
+     * Instantiates new subsystem; make ONLY ONE.
+     * <p>
+     * <code> public static final HatchPlacerSubsystem hatchPlacer = new HatchPlacerSubsystem();
+     */
+    public HatchPlacerSubsystem() {
         scissorHolder = new DoubleSolenoid(EXTRA_PCM, SCISSOR_IN, SCISSOR_OUT);
         leftLauncher = new DoubleSolenoid(EXTRA_PCM, L_LAUNCH_IN, L_LAUNCH_OUT);
         rightLauncher = new DoubleSolenoid(EXTRA_PCM, R_LAUNCH_IN, R_LAUNCH_OUT);
     }
 
-    public void lockScissors() {
+    /**
+     * Secures the hatch in place by opening the scissors.
+     */
+    public void secureHatch() {
         scissorHolder.set(Value.kReverse);
     }
 
-    public void releaseScissors() {
+    /**
+     * Releases the hatch by closing the scissors;
+     * it will hang loosely and can be knocked off.
+     */
+    public void releaseHatch() {
         scissorHolder.set(Value.kForward);
     }
 
+    /**
+     * Extend the pistons that push the hatch off the scissors.
+     * <p><b>Warning:</b> only use if the hatch is loose (scissors are closed)!
+     */
     public void extendLauncher() {
         leftLauncher.set(Value.kForward);
         rightLauncher.set(Value.kForward);
     }
 
+    /**
+     * Retract the pistons that push the hatch off the scissors.
+     */
     public void retractLauncher() {
         leftLauncher.set(Value.kReverse);
         rightLauncher.set(Value.kReverse);
     }
 
-    @Override //TODO default command
-    protected void initDefaultCommand() {}
+    @Override // TODO default command
+    protected void initDefaultCommand() {
+    }
 
 }
