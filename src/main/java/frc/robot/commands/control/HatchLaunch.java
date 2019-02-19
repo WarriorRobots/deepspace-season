74de750ac0501d0774f7e8179fa0b46786687dc4
Joshua B <dogpetkid@gmail.com>
2019-02-19 15:41:49 -0700

    Hatch launcher looping
    Rewrite the Hatch launching commands to have the pneumatics be run over 5 loop iterations (without a loop being in the commmand)

--- src/main/java/frc/robot/commands/control/HatchLaunch.java
+++ src/main/java/frc/robot/commands/control/HatchLaunch.java
@@ -7,21 +7,54 @@
 
 package frc.robot.commands.control;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 /** Launch a hatch off of the hatch placer */
-public class HatchLaunch extends InstantCommand {
+public class HatchLaunch extends Command {
   
+  /** Count variable for the loop of pneumatic */
+  private int i;
+
   public HatchLaunch() {
     super();
     requires(Robot.hatchPlacer);
   }
 
+  
   @Override
   protected void initialize() {
+    // Initialization of for loop
+    // for (Init, ---, ---) {---};
+    i = 0;
+  }
+
+  @Override
+  protected void execute() {
+    // The purpose of running the pneumatic in a loop format is to garantee the pneumatic fires
+    // (1 loop is not enough time for the pneumatic to fire)
+
+    // Execute of for loop
+    // for (---, ---, ---) {Exec};
     Robot.hatchPlacer.releaseHatch();
     Robot.hatchPlacer.extendLauncher();
+
+    // Increment of for loop
+    // for (---, ---, Inc) {---};
+    i++;
+  }
+
+  @Override
+  protected boolean isFinished() {
+    // Condition of for loop
+    // for (---, Cond, ---) {---};
+    return (i > 5);
+    // 5 is the approximate number of loops a pneumatic takes to fire
+  }
+
+  @Override
+  protected void interrupted() {
+    //XXX Alex: set to neutral
   }
 
 }
