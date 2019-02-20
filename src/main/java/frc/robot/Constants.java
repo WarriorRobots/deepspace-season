64e6ae6d4defd29bbd28122727b03fcdeedd395d
Joshua B <dogpetkid@gmail.com>
2019-02-19 17:43:03 -0700

    Encoder clicks per revolution
    Grayhill and Vex encoder clicks to degrees and back including gear ratios

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,6 +43,11 @@ public final class Constants {
 	 */
 	public static final int TIMEOUT_MS = 10;
 
+	/** Number of encoder clicks of a Grayhill encoder per revolution */
+	public static final double GRAYHILL_PER_REV = 128;
+	/** Number of encoder clicks of a Vex encoder per revolution */
+	public static final double VEX_PER_REV = 1024;
+
 	public static final double WHEEL_DIAMETER = 6;
 	public static final int CLICKS_PER_REVOLUTION = 128;
 	/** {@value} */
@@ -57,11 +62,46 @@ public final class Constants {
 	}
 
 	public static final double clicksToInches(int clicks) {
+		//TODO: better explain the INCHES_PER_CLICK and where is the radius of the tire?? (from Josh)
 		return ((double) clicks) * INCHES_PER_CLICK;
 	}
 
 	public static final int inchesToClicks(double inches) {
 		return (int) (inches / INCHES_PER_CLICK);
+	}
+
+	/**
+	 * @param ratio gear ratio in/out
+	 * @see gearbox
+	 */
+	public static final double grayhillDegreesToClicks(double degrees, double ratio) {
+		// degrees * rev/deg * enc/rev * in/out
+		return degrees / 360 * GRAYHILL_PER_REV * ratio;
+	}
+	/**
+	 * @param ratio gear ratio in/out
+	 * @see gearbox
+	 */
+	public static final double grayhillClicksToDegrees(double clicks, double ratio) {
+		// encoder * deg/rev * rev/enc * out/in
+		return clicks * 360 / GRAYHILL_PER_REV / ratio;
+	}
+
+	/**
+	 * @param ratio gear ratio in/out
+	 * @see gearbox
+	 */
+	public static final double vexDegreesToClicks(double degrees, double ratio) {
+		// degrees * rev/deg * enc/rev * in/out
+		return degrees / 360 * VEX_PER_REV * ratio;
+	}
+	/**
+	 * @param ratio gear ratio in/out
+	 * @see gearbox
+	*/
+	public static final double vexClicksToDegrees(double clicks, double ratio) {
+		// encoder * deg/rev * rev/enc * out/in
+		return clicks * 360 / VEX_PER_REV / ratio;
 	}
 
 	public static final double ftToM(double ftPerSec) {
@@ -70,6 +110,38 @@ public final class Constants {
 
 	public static final double mToFt(double mPerSec) {
 		return mPerSec * (1 / 0.3048);
+	}
+
+	/**
+	 * All gearbox ratios of important non-driving moters
+	 * All ratios should be in terms of inner rotations to outer rotations
+	 */
+	public static final class gearbox {
+		
+		/**
+		 * Cargo intake
+		 * 10:1 in:out
+		 */
+		public static final double intakeCargo = 10;
+
+		/**
+		 * Cargo arm
+		 * 300:1 in:out 
+		 */
+		public static final double arm = 300;
+
+		/**
+		 * Hatch intake
+		 * 10:1 in:out
+		 */
+		public static final double intakeHatch = 10;
+
+		/**
+		 * Elevator
+		 * 50:1 in:out
+		 */
+		public static final double elevator = 50;
+
 	}
 
 	/**
