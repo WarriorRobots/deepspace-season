0ef21312e075c612c1882494a3cfadfe3bf7af24
Joshua L Budd <dogpetkid@gmail.com>
2019-10-05 15:27:14 -0700

    Fix tabbing
    Tabbing was fixed from 2 spaces up to 4 spaces

--- src/main/java/frc/robot/util/AutoHandler.java
+++ src/main/java/frc/robot/util/AutoHandler.java
@@ -8,53 +8,53 @@ import frc.robot.commands.autonomous.paths.AutoDrive;
  */
 public class AutoHandler {
 
-  // object of this class
-  private static AutoHandler instance = null;
+    // object of this class
+    private static AutoHandler instance = null;
 
-  // Name of auto to run
-  private String autoname = null;
-  // The command to run for auto
-  private Command autocommand = null;
+    // Name of auto to run
+    private String autoname = null;
+    // The command to run for auto
+    private Command autocommand = null;
 
-  /**
-   * @return The only instance of AutoHandler
-   */
-  public static AutoHandler getInstance() {
-    if (instance == null) {
-      instance = new AutoHandler();
+    /**
+     * @return The only instance of AutoHandler
+     */
+    public static AutoHandler getInstance() {
+        if (instance == null) {
+            instance = new AutoHandler();
+        }
+        return instance;
     }
-    return instance;
-  }
 
-  /**
-   * Have AutoHandler select a case based on dashboard values
-   */
-  public void selectCase() {
-    autoname = "TESTLeft90"; // XXX temporary, have the value based on inputs from the dashboard
-    autocommand = new AutoDrive(autoname);
-  }
+    /**
+     * Have AutoHandler select a case based on dashboard values
+    */
+    public void selectCase() {
+        autoname = "TESTLeft90"; // XXX temporary, have the value based on inputs from the dashboard
+        autocommand = new AutoDrive(autoname);
+    }
 
-  /**
-   * @return The command the AutoHandler has chosen
-   */
-  public Command getCase() {
-    return autocommand;
-  }
+    /**
+     * @return The command the AutoHandler has chosen
+     */
+    public Command getCase() {
+        return autocommand;
+    }
 
-  //public void startAuto() {}
+    //public void startAuto() {}
 
-  //public void stopAuto() {}
+    //public void stopAuto() {}
 
-  /**
-   * Set AutoHandler data from dashbaord
-   */
-  public void getDashboardData () {}
+    /**
+     * Set AutoHandler data from dashbaord
+     */
+    public void getDashboardData () {}
 
-  /**
-   * Resets the data in the AutoHandler
-   */
-  public void reset() {
-    autoname = null;
-    autocommand = null;
-  }
+    /**
+     * Resets the data in the AutoHandler
+     */
+    public void reset() {
+        autoname = null;
+        autocommand = null;
+    }
 }
