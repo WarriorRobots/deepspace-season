ec158261dd4187d398cf61fdd8e1b04e226c8593
Joshua L Budd <dogpetkid@gmail.com>
2019-10-14 11:05:01 -0700

    Reduce dashboard dropdowns
    Change the number of dropdowns to select auto from 4 to 1

--- src/main/java/frc/robot/util/AutoHandler.java
+++ src/main/java/frc/robot/util/AutoHandler.java
@@ -2,7 +2,6 @@ package frc.robot.util;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.commands.autonomous.paths.AutoDrive;
-import frc.robot.commands.autonomous.paths.AutoReverse;
 import frc.robot.commands.autonomous.paths.routines.RightRocket;
 import frc.robot.commands.autonomous.paths.routines.Rocket;
 
@@ -14,8 +13,6 @@ public class AutoHandler {
     // object of this class
     private static AutoHandler instance = null;
 
-    // Name of auto to run
-    private String autoname = null;
     // The command to run for auto
     private Command autocommand = null;
 
@@ -37,88 +34,25 @@ public class AutoHandler {
     */
     public void selectCase() {
         switch (DashboardHandler.getInstance().getAutoAction()) {
-            case FORWARDS:
-                switch (DashboardHandler.getInstance().getStartingHab()) {
-                    case HAB1:
-                        autoname = "ForwardsHab1";
-                        break;
-                    case HAB2:
-                        autoname = "ForwardsHab2";
-                        break;
-                }
+            case RIGHTROCKETHAB1:
+                autocommand = new Rocket("RocketRightHab1");
                 break;
-            case ROCKET:
-                switch (DashboardHandler.getInstance().getStartingPosition()) {
-                    case LEFT:
-                        switch (DashboardHandler.getInstance().getStartingHab()) {
-                            case HAB1:
-                                autoname = "RocketLeftHab1";
-                                break;
-                            case HAB2:
-                                autoname = "RocketLeftHab2";
-                                break;
-                        }
-                        break;
-                    case RIGHT:
-                        switch (DashboardHandler.getInstance().getStartingHab()) {
-                            case HAB1:
-                                //autoname = "RocketRightHab1";
-                                autoname = null;
-                                autocommand = new Rocket("RocketRightHab1");
-                                break;
-                            case HAB2:
-                                autoname = "RocketRightHab2";
-                                break;
-                        }
-                        break;
-                    case MIDDLE:
-                        // There is currently no middle rocket auto
-                        break;
-                }
+            case RIGHTROCKETHAB2:
+                autocommand = new Rocket("RocketRightHab2");
                 break;
-            case CARGOSHIP:
-                // There is currently no cargoship autos
+            case LEFTROCKETHAB1:
+                autocommand = new Rocket("RocketLeftHab1");
                 break;
-            case NONE:
-                // There is no action, this is purposely left blank
-                break;
-        }
-
-        switch (DashboardHandler.getInstance().getTestAction()) {
-            case BACKSTRAIGHTROCKET:
-                autoname = "TESTBackStraightRocket";
+            case LEFTROCKETHAB2:
+                autocommand = new Rocket("RocketLeftHab2");
                 break;
             case FORWARDS:
-                autoname = "TESTForwards";
-                break;
-            case LEFT90:
-                autoname = "TESTLeft90";
-                break;
-            case RIGHT90:
-                autoname = "TESTRight90";
-                break;
-            case ROCKETFRONTMID:
-                autoname = "TESTRocketFrontMid";
-                break;
-            case ROCKETRIGHT:
-                autoname = "TESTRocketRight";
-                break;
-            case TURNAROUND:
-                autoname = "TESTTurnAround";
-                break;
-            case BACKSTRAIGHTROCKETWTURN:
-                autoname = null;
-                autocommand = new RightRocket();
+                autocommand = new AutoDrive("ForwardsHab1");
                 break;
             case NONE:
+                // does nothing
                 break;
         }
-
-        // Only set autocommand to something if something was chosen for it above
-        if (autoname != null) {
-            autocommand = new AutoDrive(autoname);
-        }
-        //autocommand = new AutoReverse(autoname);
     }
 
     /**
@@ -134,7 +68,6 @@ public class AutoHandler {
      * Resets the data in the AutoHandler
      */
     public void reset() {
-        autoname = null;
         autocommand = null;
     }
 
--- src/main/java/frc/robot/util/DashboardHandler.java
+++ src/main/java/frc/robot/util/DashboardHandler.java
@@ -3,19 +3,13 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.util.enums.AutoAction;
-import frc.robot.util.enums.StartingHab;
-import frc.robot.util.enums.StartingPosition;
-import frc.robot.util.enums.TestAction;
 
 public class DashboardHandler {
 
     private static DashboardHandler instance = null;
 
     // The dropdowns on the dashboard
-    private static SendableChooser<StartingHab> habDropdown;
-    private static SendableChooser<StartingPosition> positionDropdown;
     private static SendableChooser<AutoAction> actionDropdown;
-    private static SendableChooser<TestAction> testDropdown;
 
     /**
      * @return The only instance of DashboardHandler.
@@ -31,59 +25,22 @@ public class DashboardHandler {
      * Create DashboardHandler and setup dropdowns.
      */
     private DashboardHandler() { 
-        habDropdown = new SendableChooser<>();
-        positionDropdown = new SendableChooser<>();
         actionDropdown = new SendableChooser<>();
-        testDropdown = new SendableChooser<>();
-
-        // Give habDropdown options
-        habDropdown.setDefaultOption("Hab1", StartingHab.HAB1);
-        habDropdown.addOption("Hab2", StartingHab.HAB2);
-
-        // Give positionDropdown options
-        positionDropdown.setDefaultOption("Right", StartingPosition.RIGHT);
-        positionDropdown.addOption("Middle", StartingPosition.MIDDLE);
-        positionDropdown.addOption("Left", StartingPosition.LEFT);
-
+        
         // Give actionDropdown options
         actionDropdown.setDefaultOption("Forwards", AutoAction.FORWARDS);
         actionDropdown.addOption("None", AutoAction.NONE);
-        actionDropdown.addOption("Rocket", AutoAction.ROCKET);
-        actionDropdown.addOption("CargoShip", AutoAction.CARGOSHIP);
-
-        testDropdown.setDefaultOption("None", TestAction.NONE);
-        testDropdown.addOption("BackStraightRocket", TestAction.BACKSTRAIGHTROCKET);
-        testDropdown.addOption("Forwards", TestAction.FORWARDS);
-        testDropdown.addOption("Left90", TestAction.LEFT90);
-        testDropdown.addOption("Right90", TestAction.RIGHT90);
-        testDropdown.addOption("RocketFrontMid", TestAction.ROCKETFRONTMID);
-        testDropdown.addOption("RocketRight", TestAction.ROCKETRIGHT);
-        testDropdown.addOption("TurnAround", TestAction.TURNAROUND);
-        testDropdown.addOption("BackStraightRocketWTURN", TestAction.BACKSTRAIGHTROCKETWTURN);
+        actionDropdown.addOption("Right Rocket Hab1", AutoAction.RIGHTROCKETHAB1);
+        actionDropdown.addOption("Right Rocket Hab2", AutoAction.RIGHTROCKETHAB2);
+        actionDropdown.addOption("Left Rocket Hab1", AutoAction.LEFTROCKETHAB1);
+        actionDropdown.addOption("Left Rocket Hab2", AutoAction.LEFTROCKETHAB2);
     }
 
     /**
      * Put the dropdowns on the dashboard.
      */
     public void init() {
-        SmartDashboard.putData("Starting Hab selector", habDropdown);
-        SmartDashboard.putData("Position selector", positionDropdown);
         SmartDashboard.putData("Action selector", actionDropdown);
-        SmartDashboard.putData("Test case selector", testDropdown);
-    }
-
-    /**
-     * @return The selected starting Hab for auto.
-     */
-    public StartingHab getStartingHab() {
-        return habDropdown.getSelected();
-    }
-
-    /**
-     * @return The selected starting position for auto.
-     */
-    public StartingPosition getStartingPosition() {
-        return positionDropdown.getSelected();
     }
 
     /**
@@ -91,12 +48,5 @@ public class DashboardHandler {
      */
     public AutoAction getAutoAction() {
         return actionDropdown.getSelected();
-    }
-
-    /**
-     * @return The selected test case for auto.
-     */
-    public TestAction getTestAction() {
-        return testDropdown.getSelected();
     }
 }
